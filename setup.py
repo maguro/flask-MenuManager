ec05d5de6711e545219e0b0b9b392e28cb7a8d39
Alan Cabrera <acabrera@linkedin.com>
2014-08-02 10:09:15 -0700

    setup.py test is now tox

--- setup.py
+++ setup.py
@@ -16,14 +16,32 @@
 # under the License.
 #
 from io import open
+import sys
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 
-try:
-    long_description = open('README.md').read()
-except:
-    long_description = u"Easy websockets for bottle."
+class ToxCommand(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = ''
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
 
 setup(
     name='flask-MenuManager',
@@ -47,6 +65,7 @@ setup(
     ],
 
     tests_require=['tox'],
+    cmdclass={'test': ToxCommand},
 
     classifiers=[
         'Environment :: Web Environment',
